ddec5738e4fbc65200d85676043a22c23e1a6594
Jesse Marino <jessemarino2020@gmail.com>
2022-06-20 09:29:41 -0700

    updated loginform.js importing login_user from correct file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query getMe($username: String!) {
+    user(username: $username) {
+      }
+    }
+  }
+`;
+export const deleteBook = gql`
+  query 
+      }
+    }
+  }
+`;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { resolvers, typeDefs } = require("./schema");
 const { authMiddleware } = require("./utils/auth");
 
 const db = require("./config/connection");
-const routes = require("./routes");
+// const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,11 +19,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 //something
-app.use(routes);
+// app.use(routes);
 
 async function startApolloServer() {
   const httpServer = http.createServer(app);
